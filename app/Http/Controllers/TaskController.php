7d5350c96439ca65190e84599e61188b686074ea
Sushant Adhikari <83836183+SushantAdh07@users.noreply.github.com>
2025-03-19 23:37:39 +0545

    readbility

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,10 +15,7 @@ class TaskController extends Controller
     public function index(Request $request) {
         $user = $request->user();
         $users = User::latest()->get();
-    
-        // Fetch tasks based on user role
-        
-            $tasks = Task::latest()->get();
+        $tasks = Task::latest()->get();
         
     
         return Inertia::render('Components/Main', [
@@ -32,7 +29,8 @@ class TaskController extends Controller
         $task->create(
             $request->validated()
         );
-
+        session()->flash('message', 'Task added successfully!');
+        logger('Flash message set:', ['message' => session('message')]);
 
         return Inertia::location(route('index'));
     }
