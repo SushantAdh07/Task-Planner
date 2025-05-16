fa84a1f1a455a862ca0cc632f5e09b11da15d579
Sushantadh07 <sushantadhikari2828@gmail.com>
2025-05-16 23:18:26 +0545

    logics for comments submission

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,14 +6,31 @@ use App\Models\Comment;
 use App\Models\Task;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class CommentsController extends Controller
 {
-    public function comments(Task $task){
+    public function index(Task $task)
+    {
         $comments = $task->comments()->with('users')->latest()->get();
 
         return Inertia::render('Components/Main', [
             'comments' => $comments,
         ]);
+    }
+
+    public function createComments(Request $request, Task $task)
+    {
+        $request->validate([
+            'comment' => 'required|string|max:2000',
+        ]);
+
+        $comment = $task->comments()->create([
+            'comment' => $request->comment,
+            'user_id' => Auth::id()
+        ]);
+
+        return back()->with('success', 'Comment added successfully');
     }
 }
