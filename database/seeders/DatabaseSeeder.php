30547f1ef4a203e14fe875e1aa7de1f26883a0ca
Sushantadh07 <sushantadhikari2828@gmail.com>
2025-08-18 22:12:27 +0545

    seed: feeded Task table with a seed record

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Task;
 use App\Models\Team;
 use App\Models\TeamTasks;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,12 +18,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        TeamTasks::create([
+        /**TeamTasks::create([
             'member_id' => '20',
             'team_id' => '5',
             'task_name' => 'Sabita Task',
             'task_description' => 'Sabita Test of Team Task Description',
             'selected_date' => '2025-06-20'
+        ]); */
+
+        Task::create([
+            'user_id' => '5',
+            'task_name' => 'Sabita Task',
+            'task_description' => 'Sabita Test of Team Task Description',
+            'selected_date' => '2025-06-20',
+            'created_at' => Carbon::now(),
+            'updated_at'=> Carbon::now(),
         ]);
     }
 
