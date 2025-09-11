af00a7769fd80ac2e0d6f8ed6710733eb1912e7e
Sushantadh07 <sushantadhikari2828@gmail.com>
2025-09-11 15:16:53 +0545

    added: interface for repository method of crud for assigned tasks

---
+++ app/Http/Controllers/Plan/Team/AssignedTaskController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Plan\Team;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AssignedTaskController extends Controller
+{
+    //
+}
---
+++ app/Repositories/AssignedTaskRepositoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\AssignedTask;
+use Illuminate\Database\Eloquent\Collection;
+
+interface AssignedTaskRepositoryInterface
+{
+    
+    public function findById(int $id): ?AssignedTask;
+
+    
+    public function getTasksByUser(int $userId): Collection;
+
+    
+    public function getCompletedTasksByUser(int $userId): Collection;
+
+    
+    public function create(array $data): AssignedTask;
+
+    
+    public function update(int $id, array $data): bool;
+
+    
+    public function delete(int $id): bool;
+}
+
+
+    
+
