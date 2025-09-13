24fb650d49d8d6f1c8559a7f9e828f966c8a255a
Sushantadh07 <sushantadhikari2828@gmail.com>
2025-09-13 23:27:28 +0545

    refactor: assigned task

--- app/Http/Controllers/Plan/Team/AssignedTaskController.php
+++ app/Http/Controllers/Plan/Team/AssignedTaskController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Plan\Team;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AssignedTaskRequest;
+use App\Repositories\AssignedTaskRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AssignedTaskController extends Controller
 {
-    //
+    protected $assignedTaskRepository;
+
+    public function __construct(AssignedTaskRepositoryInterface $assignedTaskRepository)
+    {
+        $this->assignedTaskRepository = $assignedTaskRepository;
+    }
+
+   
+    public function store(AssignedTaskRequest $request)
+    {
+
+        $validatedData = $request->validated();
+        $member_id = Auth::guard('member')->id;
+        $validatedData['assigned_by_user_id'] = $member_id;
+        $validatedData['status'] = 'pending';
+
+        $this->assignedTaskRepository->create($validatedData);
+
+        return redirect()->back()->with('success', 'Task assigned successfully!');
+    }
 }
---
+++ app/Http/Requests/AssignedTaskRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AssignedTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id'=> ['required', 'exists:users,id'],
+            'task_id'=> ['required', 'exists:tasks,id'],
+            'team_id'=> ['required', 'exists:teams,id'],
+            'due_date'=> ['required', 'date'],
+            'priority'=> ['required', 'string', 'in:low,medium,high,critical'],
+            
+        ];
+    }
+}
